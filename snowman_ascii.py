e0c669cca891866cc53df02486568f2bd07548b4
Karim <karim.talbi7@gmail.com>
2025-10-02 15:46:51 +0200

    added snowman ascii art under snowman_ascii.py

---
+++ snowman_ascii.py
@@ -0,0 +1,29 @@
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     r"""
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     r"""
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     r"""
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     r"""
+      ___  
+     /___\\ 
+     """
+ ]
