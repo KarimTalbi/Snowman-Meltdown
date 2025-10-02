15071e45475ca1a0c7ca3a37c32c826e902675eb
Karim <karim.talbi7@gmail.com>
2025-10-02 16:29:50 +0200

    changed some code and files

---
+++ game_logic.py
@@ -0,0 +1,59 @@
+import random
+from ascii_art import STAGES
+
+# List of secret words
+WORDS = ["python", "git", "github", "snowman", "meltdown"]
+
+
+def get_random_word():
+    """Selects a random word from the list."""
+    return WORDS[random.randint(0, len(WORDS) - 1)]
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    # Display the snowman stage for the current number of mistakes.
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
+def play_game():
+    secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
+
+    print("Welcome to Snowman Meltdown!")
+
+    while True:
+        display_game_state(mistakes, secret_word, guessed_letters)
+
+        guess = input("Guess a letter: ").lower().strip()
+        print("You guessed:", guess)
+
+
+        if guess in secret_word:
+            guessed_letters.append(guess)
+
+        else:
+            mistakes += 1
+
+        if mistakes >= len(STAGES):
+            print("The snowman melted before you guessed the word")
+            print(f"The word was {secret_word}")
+            break
+
+        if len(guessed_letters) == len(secret_word):
+            print(f"You saved the snowman by guessing the word {secret_word}")
+            break
+
+        display_game_state(mistakes, secret_word, guessed_letters)
+    display_game_state(mistakes, secret_word, guessed_letters)
+
+
--- snowman.py
+++ snowman.py
@@ -1,28 +1,4 @@
-import random
-import snowman_ascii as snow
-
-# List of secret words
-WORDS = ["python", "git", "github", "snowman", "meltdown"]
-
-
-def get_random_word():
-    """Selects a random word from the list."""
-    return WORDS[random.randint(0, len(WORDS) - 1)]
-
-
-def play_game():
-    secret_word = get_random_word()
-    guessed_letters = []
-    mistakes = 0
-
-    print("Welcome to Snowman Meltdown!")
-    # For now, display the initial game state.
-    snow.display_game_state(mistakes, secret_word, guessed_letters)
-
-    # Prompt user for one guess (logic to be enhanced later)
-    guess = input("Guess a letter: ").lower()
-    print("You guessed:", guess)
-
+from game_logic import play_game
 
 if __name__ == "__main__":
     play_game()
